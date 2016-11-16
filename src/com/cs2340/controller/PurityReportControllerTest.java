7e65b1582b26237075aefbe5744b1efbcc69c5df
Josiah Smith <jsmith613@gatech.edu>
2016-11-15 19:42:26 -0500

    re-adding test file to git

---
+++ src/com/cs2340/controller/PurityReportControllerTest.java
@@ -0,0 +1,53 @@
+package com.cs2340.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Josiah Smith on 11/14/2016.
+ */
+public class PurityReportControllerTest {
+    @Test
+    public void testValLatLon() {
+        //Valid Inputs
+        String actual = PurityReportController.valLatLon(0.0, 0.0);
+        String expected = null;
+        assertEquals(
+                "Valid Inputs returned invalid result",
+                actual,
+                expected);
+
+        //Lat too large
+        actual = PurityReportController.valLatLon(91.0, 0.0);
+        expected = "latitude is too large";
+        assertEquals(
+                "Did not return correct result when Lat is too large",
+                actual,
+                expected);
+
+        //Lat too small
+        actual = PurityReportController.valLatLon(-91.0, 0.0);
+        expected = "latitude is too small";
+        assertEquals(
+                "Did not return correct result when Lat is too small",
+                actual,
+                expected);
+
+        //Lon too large
+        actual = PurityReportController.valLatLon(0.0, 181.0);
+        expected = "longitude is too large";
+        assertEquals(
+                "Did not return correct result when Lon is too large",
+                actual,
+                expected);
+
+        //Lon too small
+        actual = PurityReportController.valLatLon(0.0, -181.0);
+        expected = "longitude is too small";
+        assertEquals(
+                "Did not return correct result when Lon is too small",
+                actual,
+                expected);
+    }
+}
